2bb8f4d6d4b3fc38d7542940a8541c1edaa99c1b
ABDUR-RAZZAK-ROUZEX <abdurrazzak1663@gmail.com>
2023-12-13 23:25:01 +0600

    gitignore file added and working in the auction controller

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,6 +1,9 @@
 ﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuctionService.Controllers;
 
@@ -15,5 +18,28 @@ public class AuctionsController : ControllerBase
     {
         _context = context;
         _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    {
+        var auctions = await _context.Auctions
+        .Include(x => x.Item)
+        .OrderBy(x => x.Item.Make)
+        .ToListAsync();
+
+        return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionByid(Guid id)
+    {
+        var auction = await _context.Auctions
+        .Include(x => x.Item)
+        .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (auction == null) return NotFound();
+
+        return _mapper.Map<AuctionDto>(auction);
     }
 }
